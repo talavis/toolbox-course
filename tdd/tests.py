ef45925e519dac3dc65de91e7e4e20fcf1e93f6a
Linus Östberg <linus@talavis.eu>
2016-01-26 13:10:29 +0100

    Updated explanation for rcomplement test

--- tdd/tests.py
+++ tdd/tests.py
@@ -4,6 +4,8 @@ def rcomplement(dna) :
     '''
     Returns a string with the reverse complement of a DNA sequence.
     Sequences containing non-DNA will return an empty string.
+
+    The complements are A-T and C-G; the returned string should be reversed.
     '''
     return None
 
